f0a86657e7085c2faddcf48a31837c981a81f333
arohrberg <37849191+arohrberg@users.noreply.github.com>
2021-07-18 16:52:54 +0200

    Add files via upload
    ...gray hair incoming over this one....

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makes matrix to cache inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+      inv <- NULL
+      set <- function(y){
+        x <<- y
+        inv <<- NULL
+      }
+      get <- function(){x}
+      setInv <- function(inverse) {
+        inv <<- inverse
+      }
+      getInv <- function() {inv}
+      
+      list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
-
-## Write a short comment describing this function
+## check if cache is inverse, return if so, else do inverse input matrix and return said inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInv()
+        if(!is.null(inv))  {
+          print("cache found")
+          return(inv)
+        }
+        z <- x$get()
+        inv <- solve(z , ...)
+        x$setInv(inv)
+        inv
 }
+
+## test
+## it took me days and probably a lot of gray hairs to remember that zero-determinant matrices can never be inversed...
+## grabMatrix <- makeCacheMatrix(matrix(1:4, nrow = 2, ncol = 2)) 
+## works
+## grabMatrix$get() 
+## check if in cache
+## grabMatrix$getInv() ## NULL
+## cacheSolve(grabMatrix) ## inv 
+## cacheSolve(grabMatrix) ## print works as expected
